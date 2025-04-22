6e939a72764bc0b2cc7ad629bbdc3efb36f5f199
Ayeshafatima123 <160357945+Ayeshafatima123@users.noreply.github.com>
2025-04-23 00:51:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 import  string
 
-def generator_passward(length,use_dights,use_special):
+def generator_password(length,use_dights,use_special):
         characters = string.ascii_letters
 
         if use_dights:
@@ -13,16 +13,16 @@ def generator_passward(length,use_dights,use_special):
 
                return    ''.join(random.choice(characters) for _ in range(length))
         
-        st.title("Passward Generator")
+        st.title("Password Generator")
 
-        length =st.slider("Select Passward lenght ", min_value=6,   max_value=32,  value=12)
+        length =st.slider("Select Password lenght ", min_value=6,   max_value=32,  value=12)
 
         use_dights = st.checkbox ("Include Dights")
  
         use_special =st.checkbox("Incdude special character")
 
-        if st._bottom("Generator Passward"):
-                passwad = generator_passward(length,use_dights,use_special,)
-                st.write(f"Generator Password:`{passwad}`")
+        if st._buttom("Generator Password"):
+                passoard = generator_password(length,use_dights,use_special,)
+                st.write(f"Generator Password:`{password}`")
 
                 st.write("----")
